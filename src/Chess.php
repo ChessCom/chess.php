ce4ad1f0030eaf14da7a80333ba9609cd5287619
Saša Stamenković <umpirsky@gmail.com>
2022-04-26 10:29:53 +0200

    CV-265550 Array and string offset access syntax with curly braces is no longer supported

--- src/Chess.php
+++ src/Chess.php
@@ -193,7 +193,7 @@ class Chess
 		$position = $tokens[0];
 		$square = 0;
 		for ($i = 0; $i < strlen($position); $i++) {
-			$piece = $position{$i};
+			$piece = $position[$i];
 			if($piece === '/') {
 				$square += 8;
 			}
@@ -332,14 +332,14 @@ class Chess
 			$sumFields = 0;
 			$previousWasNumber = false;
 			for ($k = 0; $k < strlen($rows[$i]); $k++) {
-				if (ctype_digit($rows[$i]{$k})) {
+				if (ctype_digit($rows[$i][$k])) {
 					// 8th criterion: every row is valid
 					if ($previousWasNumber) return ['valid' => false, 'error_number' => 8, 'error' => $errors[8]];
-					$sumFields += intval($rows[$i]{$k}, 10);
+					$sumFields += intval($rows[$i][$k], 10);
 					$previousWasNumber = true;
 				} else {
 					// 9th criterion: check symbols of piece
-					if (strpos(self::SYMBOLS, $rows[$i]{$k}) === false) return ['valid' => false, 'error_number' => 9, 'error' => $errors[9]];
+					if (strpos(self::SYMBOLS, $rows[$i][$k]) === false) return ['valid' => false, 'error_number' => 9, 'error' => $errors[9]];
 					$sumFields++;
 					$previousWasNumber = false;
 				}
@@ -351,8 +351,8 @@ class Chess
 
 		// 11th criterion: en-passant if last is black's move, then its must be white turn
 		if (strlen($tokens[3]) > 1) {
-			if (($tokens[3]{1} == '3' && $tokens[1] == 'w') ||
-				($tokens[3]{1} == '6' && $tokens[1] == 'b')) {
+			if (($tokens[3][1] == '3' && $tokens[1] == 'w') ||
+				($tokens[3][1] == '6' && $tokens[1] == 'b')) {
 				return ['valid' => false, 'error_number' => 11, 'error' => $errors[11]];
 			}
 		}
